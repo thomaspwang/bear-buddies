61e362aa4f6e1a8fd5cb1051393cac04e3018065
bruno <bruno.vieira@berkeley.edu>
2023-03-22 01:17:47 -0700

    finished login

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import '@/styles/login.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+  )
 }
---
+++ src/pages/login.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Form from 'react-bootstrap/Form';
+import {Button} from "react-bootstrap";
+
+
+
+
+function LogInPage() {
+    return (
+        <div className="container">
+            <div className="page-title">
+                <h1>Log In to Bear Buddies</h1>
+            </div>
+            <div className="form-container">
+                <Form className="form-style">
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>email</Form.Label>
+                        <Form.Control type="email" placeholder="enter email" />
+                        
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>password</Form.Label>
+                        <Form.Control type="password" placeholder="password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" className="submit-btn">
+                        Log In
+                    </Button>
+                </Form>
+            </div>
+            <div className="line"></div>
+            <div className="not-member">
+                <h5 className="col-item-member">not a member?</h5>
+                <Button variant="primary" type="submit" className="col-item-member">
+                    join the community!
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default LogInPage;
+
+/*
+NOTES:
+    Use React-Bootstrap components (I think they’re called forms?)
+    Feel free to make a modular component called “Login Button” but you also don’t have to
+    Use React-Bootstrap for dropdowns
+*/
