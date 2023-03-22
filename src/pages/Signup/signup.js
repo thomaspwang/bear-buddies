9794216fd777b77215a482c34e6cf74c32f930ed
bruno <bruno.vieira@berkeley.edu>
2023-03-22 02:16:02 -0700

    working on signup page

---
+++ src/pages/Signup/signup.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SignUpPage() {
+    return (
+        <div className="container">
+            <div className="signup-title">
+                <h1>Sign Up for Bear Buddies</h1>
+            </div>
+            <div>
+                <h4>Create a free account or <a className="blue-txt" href="../Login/login">login</a></h4>
+            </div>
+        </div>
+    )
+}
+
+export default SignUpPage;
