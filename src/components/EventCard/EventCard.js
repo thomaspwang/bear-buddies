23f85aa37af23b79e91ed76d8a9d0b6e6bf7509a
Dylan Hopkins <dylanhopkins@berkeley.edu>
2023-03-22 11:24:11 -0700

    finished hardcoded card

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './EventCard.module.css'
-import { Card } from "react-bootstrap";
+import styles from './EventCard.module.css';
+import { Button } from 'react-bootstrap';
 import React from "react";
 import Image from 'next/image'
 
@@ -30,8 +30,25 @@ export default function EventCard() {
                             </svg>
                             <p id={styles.para}>Wednesday, March 15, 10:00am</p>
                         </div>
-                        
+                        <div className={styles.iconText}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24">
+                                <path fill="#CBCBCB" d="M12 12q.825 0 1.413-.588T14 10q0-.825-.588-1.413T12 8q-.825 0-1.413.588T10 10q0 .825.588 1.413T12 12Zm0 7.35q3.05-2.8 4.525-5.088T18 10.2q0-2.725-1.738-4.462T12 4Q9.475 4 7.737 5.738T6 10.2q0 1.775 1.475 4.063T12 19.35ZM12 22q-4.025-3.425-6.012-6.362T4 10.2q0-3.75 2.413-5.975T12 2q3.175 0 5.588 2.225T20 10.2q0 2.5-1.988 5.438T12 22Zm0-11.8Z"/>
+                            </svg>
+                            <p id={styles.para}>Moffitt Library</p>
+                        </div>
+                        <div className={styles.iconText}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 16 16">
+                                <path fill="#cbcbcb" fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm4-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5z"/>
+                            </svg>
+                            <p id={styles.para}>Come study with me for this midterm! Currently stuck on heapification. If you have any advice I'd really appreciate it, I have no idea what i'm doing</p>
+                        </div>
                     </div>
+                </div>
+                <div className={styles.bottom}>
+                    <div></div>
+                    <Button className={styles.button}>
+                        <p id={styles.buttonText}>Join!</p>
+                    </Button>
                 </div>
             </div>
         </div>
