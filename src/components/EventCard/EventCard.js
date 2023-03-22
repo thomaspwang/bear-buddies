676f511dd956220d41c30f44ad540d6c780009cb
Dylan Hopkins <dylanhopkins@berkeley.edu>
2023-03-21 23:29:48 -0700

    photo not rendering

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,17 +1,20 @@
-import Boba from "@/assets/boba.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card } from "react-bootstrap";
 import React from "react";
 
 export default function EventCard() {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <h1 className="head">Test</h1>
-                <Card.Text className="primary">
-                    Description: testing stuff
-                </Card.Text>
-            </Card.Body>
-            <Card.Img variant="bottom" src={Boba} />
-        </Card>
+        <div>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <h1 className="head">Test</h1>
+                    <Card.Text className="primary">
+                        Description: testing stuff
+                    </Card.Text>
+                </Card.Body>
+                <Card.Img variant="bottom" src='/src/assets/boba.png' alt="Image Broke" />
+            </Card>
+        </div>
+        
     )
 }
