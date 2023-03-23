369f57450f01e01a72a7919dd06a3d8fd8556226
Namita Khanchustambham <nkhanchu@berkeley.edu>
2023-03-23 00:50:12 -0700

    Event Button revised intital layout

--- src/components/EventButton/EventButton.js
+++ src/components/EventButton/EventButton.js
@@ -3,19 +3,20 @@ import {Button} from "react-bootstrap";
 import styles from "./EventButton.module.css";
 import icon from "../../public/ph_note-pencil-bold.svg";
 import Image from 'next/image';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function EventButton({text}) {
+    EventButton.propTypes = {
+        text: String,
+    }
+        
     return (
-        <Button variant="primary" type="submit" className={styles.textEdit} > 
+        <Button variant="primary" type="submit" className={styles.textEdit}> 
              <Image src={icon}/> 
-             
-            {text}
+             {text}
          </Button>
     )
 }
 
 export default EventButton;
-
-
-
 
