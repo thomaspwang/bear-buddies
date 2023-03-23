83793daa76896da38660928acb6a30e386c3a48f
bruno <bruno.vieira@berkeley.edu>
2023-03-22 18:05:56 -0700

    login

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,33 +1,33 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './login.module.css';
 
 function LogInPage() {
     return (
-        <div className="container">
-            <div className="page-title">
+        <div className={styles.container}>
+            <div className={styles.page_title}>
                 <h1>Log In to Bear Buddies</h1>
             </div>
-            <div className="form-container">
-                <Form className="form-style">
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+            <div className={styles.form_container}>
+                <Form className={styles.form_style}>
+                    <Form.Group className={styles.mb_3} controlId="formBasicEmail">
                         <Form.Label>email</Form.Label>
                         <Form.Control type="email" placeholder="enter email" />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Group className={styles.mb_3} controlId="formBasicPassword">
                         <Form.Label>password</Form.Label>
                         <Form.Control type="password" placeholder="password" />
                     </Form.Group>
-                    <Button variant="primary" type="submit" className="submit-btn">
+                    <Button variant="primary" type="submit" className={styles.submit_btn}>
                         Log In
                     </Button>
                 </Form>
             </div>
-            <div className="line"></div>
-            <div className="not-member-container">
-                <h5 className="col-item-member">not a member?</h5>
-                <Button variant="primary" type="submit" className="col-item-member-btn" href="../Signup/signup">
+            <div className={styles.line}></div>
+            <div className={styles.not_member_container}>
+                <h5 className={styles.col_item_member}>not a member?</h5>
+                <Button variant="primary" type="submit" className={styles.col_item_member_btn} href="../Signup/signup">
                     join the community!
                 </Button>
             </div>
