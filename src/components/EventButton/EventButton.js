c8ba5bd8c9dbb4817f8c749050476cd99fb67d48
Namita Khanchustambham <nkhanchu@berkeley.edu>
2023-03-23 17:44:08 -0700

    Fixed PR Round 1 comments

--- src/components/EventButton/EventButton.js
+++ src/components/EventButton/EventButton.js
@@ -1,9 +1,8 @@
 import React from "react";
 import {Button} from "react-bootstrap";
+import Image from 'next/image';
 import styles from "./EventButton.module.css";
 import icon from "../../public/ph_note-pencil-bold.svg";
-import Image from 'next/image';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function EventButton({text}) {
     EventButton.propTypes = {
