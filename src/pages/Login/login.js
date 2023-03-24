c3c9ecda9a97bc66dd0136efe87db519649011ef
bruno <bruno.vieira@berkeley.edu>
2023-03-23 23:39:25 -0700

    fixed bugs

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
-import styles from './login.module.css';
+import styles from './Login.module.css';
 
 function LogInPage() {
     return (
