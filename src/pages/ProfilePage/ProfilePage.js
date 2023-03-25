55f874a7e858ecf3dfb17452b38752c429ac6b99
hyeseung <hyeseungsong@Hyeseungs-MacBook-Pro-2.local>
2023-03-25 16:03:44 -0700

    created new profile page files with the same finished code

---
+++ src/pages/ProfilePage/ProfilePage.js
@@ -0,0 +1,60 @@
+import React from "react";
+import './ProfilePage.module.css';
+import styles from './ProfilePage.module.css'
+import Image from 'next/image';
+
+function ProfilePage() {
+    <img className={styles.image} src = 'tommy.png'></img>
+    return (
+        <div className="container">
+            <div className={styles.image}>
+                <div className={styles.circularImage} />
+            </div>
+            <div className={styles.profileR}>
+                <div className={styles.first}>
+                    <span>
+                        <span className={styles.bold}>First Name</span>
+                    </span>
+                    <span className={styles.fn}> Tommy</span>
+                </div>
+                <div className={styles.last}>
+                    <span>
+                        <span className={styles.bold}>Last Name</span>
+                    </span>
+                    <span className={styles.ln}> Wang</span>
+                </div>
+                <div className={styles.gender}>
+                    <span>
+                        <span className={styles.bold}>Gender</span>
+                    </span>
+                    <span className={styles.gn}> Male</span>
+                </div>
+                <div className={styles.graduation}>
+                    <span>
+                        <span className={styles.bold}>Graduation Year</span>
+                    </span>
+                    <span className={styles.gradn}> 2024</span>
+                </div>
+                <div className={styles.major}>
+                    <span>
+                        <span className={styles.bold}>Major</span>
+                    </span>
+                    <span className={styles.mn}> Computer Science</span>
+                </div>
+                <div className={styles.email}>
+                    <span>
+                        <span className={styles.bold}>Email</span>
+                    </span>
+                    <span className={styles.en}> thomaswang@berkeley.edu</span>
+                </div>
+                <div className={styles.number}>
+                    <span>
+                        <span className={styles.bold}>Phone Number</span>
+                    </span>
+                    <span className={styles.nn}> (510) 701-3797</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ProfilePage;
