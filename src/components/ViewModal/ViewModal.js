205e17ce4a44c6cb3bbdbaa7fb1d9d6376e70b73
Leon <leonjia0924@gmail.com>
2023-03-30 00:42:53 -0700

    fixed names on css

--- src/components/ViewModal/ViewModal.js
+++ src/components/ViewModal/ViewModal.js
@@ -7,9 +7,9 @@ import styles from './ViewModal.module.css';
 function ViewModal({ show, onClose, image, title, time, location, description, mem_limit, mem_array}) {
   const List = () => {
     const listItems = mem_array.map(person =>
-      <div className={styles.iconText}>
+      <div className={styles.icon_text}>
         <img className={styles.pfp} src="tommy.jpg"/>
-        <div className = {styles.listText}>{person}</div>
+        <div className = {styles.list_text}>{person}</div>
       </div>
     );
     return <ul className = {styles.list}>{listItems}</ul>;
@@ -26,24 +26,24 @@ return (
     >
         <Modal.Body>
           <div>
-              <CloseButton className = {styles.closeButton} onClick={onClose}/>
-              <img className={styles.mainImage} src={image}/>
+              <CloseButton className = {styles.close_button} onClick={onClose}/>
+              <img className={styles.main_image} src={image}/>
               <h1 className = {styles.title}>{title}</h1>      
           </div>
-          <div className = {styles.bodyLeft}>
-              <div className = {styles.normalText}><b>When:</b> {time}</div>
-              <div className = {styles.normalText}><b>Where:</b> {location}</div>
-              <div className = {styles.normalText}><b>Description:</b> {description}</div>
-              <div className = {styles.normalText}><b>Activity Requirements:</b></div> 
-              <div className = {styles.normalText}>Check Allergy Info!</div>
+          <div className = {styles.body_left}>
+              <div className = {styles.normal_text}><b>When:</b> {time}</div>
+              <div className = {styles.normal_text}><b>Where:</b> {location}</div>
+              <div className = {styles.normal_text}><b>Description:</b> {description}</div>
+              <div className = {styles.normal_text}><b>Activity Requirements:</b></div> 
+              <div className = {styles.normal_text}>Check Allergy Info!</div>
           </div>
-          <div className = {styles.bodyRight}>
-            <div className = {styles.memberText}>
-              <div className = {styles.normalText}><b>Current Members ({mem_array.length}/{mem_limit})</b></div>
+          <div className = {styles.body_right}>
+            <div className = {styles.member_text}>
+              <div className = {styles.normal_text}><b>Current Members ({mem_array.length}/{mem_limit})</b></div>
             </div> 
             <List/>
             <div>
-              <button className = {styles.joinButton}>Join!</button>
+              <button className = {styles.join_button}>Join!</button>
             </div>
           </div>
         </Modal.Body>
