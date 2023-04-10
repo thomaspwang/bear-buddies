8037c7ecd028e46f89d6797c6d804682ca5e8ef1
hyeseung <hyeseungsong@Hyeseungs-MacBook-Pro-2.local>
2023-04-09 22:26:38 -0700

    added js file

--- src/pages/GroupManagementPage/GroupManagementPage.js
+++ src/pages/GroupManagementPage/GroupManagementPage.js
@@ -1,96 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import CardGrid from "@/components/CardGrid/CardGrid";
+import EventButton from "@/components/EventButton/EventButton";
+import NavBar from "@/components/NavBar/NavBar";
 import React from "react";
-import Image from 'next/image'
-import Button from 'react-bootstrap/Button';
-import Badge from '../../../public/badge';
-import Clock from '../../../public/clock';
-import Location from '../../../public/location';
-import Paragraph from '../../../public/paragraph';
-import Person from '../../../public/person';
-import styles from './EventCard.module.css';
+import styles from './GroupManagementPage.module.css';
 
-
-function OwnerBadge() {
+function GroupManagementPage( {data} ) {
     return (
-        <div className={styles.iconText}>
-            <Badge />
-            <p id={styles.badgeText}>Your Group</p>
-        </div>
-    )
-}
-
-function EventCard( {event} ) {
-    EventCard.propTypes = {
-        event: PropTypes.shape({
-            title: PropTypes.string,
-            image: PropTypes.string,
-            author: PropTypes.string,
-            authorPic: PropTypes.string,
-            maxCap: PropTypes.number,
-            currCap: PropTypes.number,
-            location: PropTypes.string,
-            time: PropTypes.string,
-            description: PropTypes.string,
-            joined: PropTypes.bool,
-            owner: PropTypes.bool,
-        }).isRequired,
-    }
-
-    let buttonText;
-    if (event.joined) {
-        buttonText = "Happening Soon!";
-    } else {
-        buttonText = "Join!"
-    }
-
-    let badge;
-    if (event.owner) {
-        badge = <OwnerBadge />
-    } else {
-        badge = <div />
-    }
-
-    return (
-        <div>
-            <div className={styles.active}>
-                <div className={styles.top}>
-                    <h1 className='head' id={styles.title}>{event.title}</h1>
-                    <div className={styles.iconText}>
-                        <Person />
-                        <p>{event.currCap}/{event.maxCap}</p>
-                    </div>
-                </div>
-                <div className={styles.body}>
-                    <Image className={styles.mainImage} src={event.image} alt="lib" width="134" height="134"/>
-                    <div className={styles.info}>
-                        <div className={styles.iconText}>
-                            <Image className={styles.pfp} src={event.authorPic} alt="face" width="21" height="21" />
-                            <p id={styles.para}>{event.author}</p>
-                        </div>
-                        <div className={styles.iconText}>
-                            <Clock />
-                            <p id={styles.para}>{event.time}</p>
-                        </div>
-                        <div className={styles.iconText}>
-                            <Location />
-                            <p id={styles.para}>{event.location}</p>
-                        </div>
-                        <div className={styles.iconText}>
-                            <Paragraph />
-                            <p id={styles.para}>{event.description}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.bottom}>
-                    {badge}
-                    <Button className={styles.button}>
-                        <p id={styles.buttonText}>{buttonText}</p>
-                    </Button>
-                </div>
+        <>
+            <NavBar />
+            <div className={styles.container}>
+                <EventButton text={"Create"}/>
             </div>
-        </div>
-        
+            <CardGrid cardData={data}/>
+        </>
     )
 }
 
-export default EventCard;
+export default GroupManagementPage;
